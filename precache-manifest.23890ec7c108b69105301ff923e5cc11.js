aa9e6c482e59f80536d46399b9768d7a74c2fc95
jiachengli1999 <35944405+jiachengli1999@users.noreply.github.com>
2020-06-11 21:24:12 -0400

    Updates

---
+++ precache-manifest.23890ec7c108b69105301ff923e5cc11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5974189563de06ea78787f9e9ca4d12a",
+    "url": "/Compass-Clock-using-React/index.html"
+  },
+  {
+    "revision": "0280ae4f0cd017c89de0",
+    "url": "/Compass-Clock-using-React/static/css/main.913387db.chunk.css"
+  },
+  {
+    "revision": "2bd28affba79f7093f82",
+    "url": "/Compass-Clock-using-React/static/js/2.90018e97.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Compass-Clock-using-React/static/js/2.90018e97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0280ae4f0cd017c89de0",
+    "url": "/Compass-Clock-using-React/static/js/main.40f67f2d.chunk.js"
+  },
+  {
+    "revision": "c47b3297863424c57c61",
+    "url": "/Compass-Clock-using-React/static/js/runtime-main.e94f44d9.js"
+  }
+]);
